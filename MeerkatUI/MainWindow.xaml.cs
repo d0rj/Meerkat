41d7e2162a25a1fd98e3d4a50593506e401bc256
d0rj <hanaan@internet.ru>
2021-01-24 19:32:22 +0300

    Added base for variables dictionary edit.

--- MeerkatUI/MainWindow.xaml.cs
+++ MeerkatUI/MainWindow.xaml.cs
@@ -1,25 +1,48 @@
 ﻿using Meerkat.Library;
-using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using MeerkatUI.Utils;
 
 
 namespace MeerkatUI
 {
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
 		private TemplateEngine engine;
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName] string prop = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+
+
+		private ObservableDictionary<string, string> variables;
+		public ObservableDictionary<string, string> Variables 
+		{
+			get => variables;
+			set
+			{
+				variables = value;
+				OnPropertyChanged("Variables");
+			} 
+		}
 
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			var variables = new Dictionary<string, string>()
+			Variables = new ObservableDictionary<string, string>()
 			{
 				{"X", "коммунизм" },
 			};
 
-			engine = new TemplateEngine(variables);
+			engine = new TemplateEngine(Variables.ToDictionary());
+
+			DataContext = this;
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
