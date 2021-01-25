903d8d08e5300aa2e926c6a3d0c3f5aa0348898f
d0rj <hanaan@internet.ru>
2021-01-25 18:15:38 +0300

    Now you can change variables by window, add and remove.

--- MeerkatUI/MainWindow.xaml.cs
+++ MeerkatUI/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using Meerkat.Library;
 using MeerkatUI.Utils;
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Controls;
 
 
 namespace MeerkatUI
@@ -19,28 +23,26 @@ namespace MeerkatUI
 		}
 
 
-		private ObservableDictionary<string, string> variables;
-		public ObservableDictionary<string, string> Variables 
+		private ObservableCollection<Variable> variables = new ObservableCollection<Variable>()
+		{
+			new Variable("Y", "утка"),
+		};
+		public ObservableCollection<Variable> Variables 
 		{
 			get => variables;
-			set
+			set 
 			{
 				variables = value;
 				OnPropertyChanged("Variables");
-			} 
-		}
+			}
+		} 
 
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			Variables = new ObservableDictionary<string, string>()
-			{
-				{"X", "коммунизм" },
-			};
-
-			engine = new TemplateEngine(Variables.ToDictionary());
+			engine = new TemplateEngine();
 
 			DataContext = this;
 		}
@@ -48,6 +50,9 @@ namespace MeerkatUI
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
+			foreach (var variable in Variables)
+				engine.Variables.Add(variable.Name, variable.Value);
+
 			var text = engine.ProcessTemplate(InputTextbox.Text);
 
 			OutputTextbox.Text = text;
@@ -63,6 +68,21 @@ namespace MeerkatUI
 		private void IgnoreUnknown_Unchecked(object sender, RoutedEventArgs e)
 		{
 			engine.IgnoreUnknown = false;
+		}
+
+
+		private void AddVariable(object sender, RoutedEventArgs e)
+		{
+			Variables.Add(new Variable("A", "яблоко"));
+		}
+
+
+		private void DeleteVariable(object sender, RoutedEventArgs e)
+		{
+			var variable = ((Button)sender).DataContext as Variable;
+
+			if (variable != null)
+				Variables.Remove(variable);
 		}
 	}
 }
---
+++ MeerkatUI/Utils/Variable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MeerkatUI.Utils
+{
+	public sealed class Variable
+	{
+		public string Name { get; set; }
+		public string Value { get; set; }
+
+
+		public Variable(string name, string value)
+		{
+			Name = name;
+			Value = value;
+		}
+	}
+}
