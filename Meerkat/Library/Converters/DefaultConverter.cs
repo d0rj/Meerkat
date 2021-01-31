3c6475553a6b7846671e41f72eac8f55b312b458
d0rj <hanaan@internet.ru>
2021-01-31 18:53:06 +0300

    Refactoring: language case parsing moved to separated static class.

--- Meerkat/Library/Converters/DefaultConverter.cs
+++ Meerkat/Library/Converters/DefaultConverter.cs
@@ -1,8 +1,7 @@
 ﻿using LingvoNET;
 
+using Meerkat.Library.Converters.Utils;
 using Meerkat.Library.Exceptions;
-
-using System.Collections.Generic;
 
 
 namespace Meerkat.Library.Converters
@@ -13,31 +12,10 @@ namespace Meerkat.Library.Converters
 		protected readonly Number number;
 
 
-		protected static Dictionary<string, Case> commands = 
-			new Dictionary<string, Case>()
-			{
-				{"имен", Case.Nominative },
-				{"родит", Case.Genitive },
-				{"дате",  Case.Dative},
-				{"вини", Case.Accusative },
-				{"твор", Case.Instrumental },
-				{"предл", Case.Locative },
-
-				{string.Empty, Case.Nominative },
-
-				{"именительный", Case.Nominative },
-				{"родительный", Case.Genitive },
-				{"дательный",  Case.Dative},
-				{"винительный", Case.Accusative },
-				{"творительный", Case.Instrumental },
-				{"предложный", Case.Locative },
-			};
-
-
 		public DefaultConverter(string command, string number = "")
 		{
-			if (commands.ContainsKey(command))
-				convertCase = commands[command];
+			if (CaseParser.IsValid(command))
+				convertCase = CaseParser.Parse(command);
 			else
 				throw new UnknownFormException(command, "for nouns");
 
---
+++ Meerkat/Library/Converters/Utils/CaseParser.cs
@@ -0,0 +1,42 @@
+﻿using LingvoNET;
+using Meerkat.Library.Exceptions;
+using System.Collections.Generic;
+
+
+namespace Meerkat.Library.Converters.Utils
+{
+	public static class CaseParser
+	{
+		private static Dictionary<string, Case> commands =
+			new Dictionary<string, Case>()
+			{
+				{"имен", Case.Nominative },
+				{"родит", Case.Genitive },
+				{"дате",  Case.Dative},
+				{"вини", Case.Accusative },
+				{"твор", Case.Instrumental },
+				{"предл", Case.Locative },
+
+				{string.Empty, Case.Nominative },
+
+				{"именительный", Case.Nominative },
+				{"родительный", Case.Genitive },
+				{"дательный",  Case.Dative},
+				{"винительный", Case.Accusative },
+				{"творительный", Case.Instrumental },
+				{"предложный", Case.Locative },
+			};
+
+
+		public static bool IsValid(string command) => commands.ContainsKey(command);
+
+
+		public static Case Parse(string command)
+		{
+			if (commands.ContainsKey(command))
+				return commands[command];
+
+			throw new UnknownFormException(command);
+		}
+	}
+}
