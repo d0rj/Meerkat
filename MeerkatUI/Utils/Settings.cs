a51bac28d71186c312d2b13c598a5f0c5736bad4
d0rj <hanaan@internet.ru>
2021-01-31 18:54:25 +0300

    Changed default font size.

--- MeerkatUI/Utils/Settings.cs
+++ MeerkatUI/Utils/Settings.cs
@@ -5,8 +5,8 @@ namespace MeerkatUI.Utils
 {
 	public sealed class Settings : ICloneable
 	{
-		public int EditorFontSize { get; set; } = 12;
-		public int OutputFontSize { get; set; } = 12;
+		public int EditorFontSize { get; set; } = 16;
+		public int OutputFontSize { get; set; } = 16;
 
 
 		/// <summary> Deep copy </summary>
