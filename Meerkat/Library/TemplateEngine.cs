cdf6b4aa8eb6643c472b23e617c2573711c16bc3
d0rj <hanaan@internet.ru>
2021-02-01 13:10:35 +0300

    Now modifiers can be separated from '+' by spaces and tabs.

--- Meerkat/Library/TemplateEngine.cs
+++ Meerkat/Library/TemplateEngine.cs
@@ -12,7 +12,7 @@ namespace Meerkat.Library
 	{
 		private readonly List<IWordMorpher> wordMorphers;
 
-		public readonly string GeneralRegex = @"\[[\t ]*([A-Z_-]+)[\t ]*[|]{0,1}[\t ]*([a-zа-я]*)[\t ]*(\+){0,1}([FMN]){0,1}\]";
+		public readonly string GeneralRegex = @"\[[\t ]*([A-Z_-]+)[\t ]*[|]{0,1}[\t ]*([a-zа-я]*)[\t ]*(\+){0,1}[\t ]*([FMN]){0,1}\]";
 
 		public bool IgnoreUnknown { get; set; }
 		public Dictionary<string, string> Variables { get; set; }
